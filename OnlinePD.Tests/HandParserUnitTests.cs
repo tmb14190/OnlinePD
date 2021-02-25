5bbdedb9c6872c1f23cd6b7841cdf774f7e3dd04
tmb14190 <Jack.moore.bd@btinternet.com>
2021-02-25 14:28:30 +0000

    Add Hand integration test with database use (#1)
    * Add hand integration test using database
    
    * Duplicate test HandIntegrationTest fixed

--- OnlinePD.Tests/HandParserUnitTests.cs
+++ OnlinePD.Tests/HandParserUnitTests.cs
@@ -95,29 +95,4 @@ namespace OnlinePD.Tests
 
         }
     }
-
-    public class HandIntegrationTests
-    {
-        [Fact]
-        public void HandIntegrationTest()
-        {
-            // arrange
-            string rawLines = File.ReadAllText("exampleHHMultiple.txt");
-            IList<string> rawHands = rawLines.Split("\r\n\r\n\r\n");
-            int expectedNumber = 3;
-            string expectedID0 = "Pokerstars #197638875182";
-            string expectedID1 = "Pokerstars #197638875183";
-            string expectedID2 = "Pokerstars #197638875184";
-
-            // act
-            IList<Hand> handsResult = rawHands.Select(hand => Hand.Parse(hand.Split("\n"))).ToList();
-
-            // assert
-            Assert.Equal(handsResult.Count, expectedNumber);
-            Assert.Equal(handsResult[0].ID, expectedID0);
-            Assert.Equal(handsResult[1].ID, expectedID1);
-            Assert.Equal(handsResult[2].ID, expectedID2);
-
-        }
-    }
 }
